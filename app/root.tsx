fa255a87c4a916b2b464b9bb84e476a8a08cf369
afflorld <sl.adamec@gmail.com>
2024-06-09 14:32:55 +0200

    modified:   app/root.tsx 	modified:   app/routes/_index.tsx 	new file:   app/tailwind.css 	modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	new file:   tailwind.config.ts

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type { LinksFunction } from "@remix-run/node"; // or cloudflare/deno
 import {
   Links,
   Meta,
@@ -5,6 +6,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+
+import styles from "./tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
 
 export default function App() {
   return (
