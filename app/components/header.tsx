90bc1b4c0dc3496707e5cf7284745f6e3e03503f
afflorld <sl.adamec@gmail.com>
2024-06-09 18:40:53 +0200

    modified:   app/components/header.tsx 	modified:   app/routes/_index.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,14 +2,14 @@ export function Header(){
 
     return(
 
-        <div className="header absolute right-16 translate-y-14 shadow-lg border-none rounded-3xl overflow-hidden bg-white z-10">
-            <nav className="flex flex-wrap flex-row list-none text-lg">
-                <li className="p-4 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">O nas</li>
-                <li className="p-4 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">Zavodni plavci</li>
-                <li className="p-4 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">Kurzy a primetske tabory</li>
-                <li className="p-4 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">Treneri</li>
-                <li className="p-4 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">Kontakt</li>
-                <li className="p-4 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">Facebook</li>
+        <div className="header absolute right-12 translate-y-16 shadow-lg border-none rounded-3xl overflow-hidden bg-white z-10">
+            <nav className="flex flex-wrap flex-row list-none text-xl">
+                <li className="p-6 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">O nas</li>
+                <li className="p-6 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">Zavodni plavci</li>
+                <li className="p-6 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">Kurzy a primetske tabory</li>
+                <li className="p-6 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">Treneri</li>
+                <li className="p-6 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">Kontakt</li>
+                <li className="p-6 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">Facebook</li>
             </nav>
         </div>
 
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,8 +14,8 @@ export default function Index() {
 
   return (
    <div className="desktop bg-white w-full h-full relative select-none overflow-x-hidden">
-        <div className="logo absolute translate-x-16 translate-y-12 border-none rounded-full overflow-hidden z-10">
-          <img className="w-36 h-36" src="/images/logo/logo.png"/>
+        <div className="logo absolute translate-x-12 translate-y-6 border-none rounded-full overflow-hidden z-10">
+          <img className="w-44 h-44" src="/images/logo/logo.png"/>
         </div>
         <Header />
         <Slideshow />
