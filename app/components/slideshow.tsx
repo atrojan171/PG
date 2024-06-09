f3690ed1d85c4dcc01673dcec44cd442f19e8d29
afflorld <sl.adamec@gmail.com>
2024-06-09 19:34:06 +0200

    modified:   app/components/header.tsx 	modified:   app/components/slideshow.tsx

--- app/components/slideshow.tsx
+++ app/components/slideshow.tsx
@@ -31,7 +31,7 @@ export function Slideshow(){
             </div>
             <div className="absolute bottom-0 z-10 flex flex-row flex-nowrap justify-self-center">
                 {data.map((d:any) => (
-                    <div className="dot w-8 h-8 m-2 bg-gray-800 border-2 rounded-3xl cursor-pointer hover:bg-blue-600 transition-all">
+                    <div className="dot w-8 h-8 m-2 bg-gray-800 border-2 rounded-3xl cursor-pointer hover:bg-blue-600 transition-all shadow-xl">
                     </div>
                 ))}
             </div>
