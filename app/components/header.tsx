f5e3b5d7001b8dc9668f3a636649313b167c5d36
afflorld <sl.adamec@gmail.com>
2024-06-09 18:08:04 +0200

    modified:   app/components/header.tsx 	modified:   app/routes/_index.tsx 	new file:   public/images/slideshow/2.jpg

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ export function Header(){
 
     return(
 
-        <div className="header absolute right-16 translate-y-14 shadow-lg border-none rounded-3xl overflow-hidden bg-white">
+        <div className="header absolute right-16 translate-y-14 shadow-lg border-none rounded-3xl overflow-hidden bg-white z-10">
             <nav className="flex flex-wrap flex-row list-none text-lg">
                 <li className="p-4 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">O nas</li>
                 <li className="p-4 hover:bg-blue-500 hover:text-white cursor-pointer transition-all">Zavodni plavci</li>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,8 +14,8 @@ export default function Index() {
 
   return (
    <div className="desktop bg-white w-full h-full relative select-none">
-        <div className="logo absolute translate-x-16 translate-y-10 border-none rounded-full overflow-hidden">
-          <img className="w-28 h-28" src="/images/logo/logo.png"/>
+        <div className="logo absolute translate-x-16 translate-y-12 border-none rounded-full overflow-hidden z-10">
+          <img className="w-36 h-36" src="/images/logo/logo.png"/>
         </div>
         <Header />
         <Slideshow />
