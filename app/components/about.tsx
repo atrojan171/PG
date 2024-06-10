41374f64192b5354c7e667767c5d40399a99c1b0
afflorld <sl.adamec@gmail.com>
2024-06-10 16:27:04 +0200

    modified:   app/components/about.tsx

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -14,47 +14,53 @@ export function About(){
         <div>
             <Parallax pages={2}>          
                 <div className="grid relative">
-                    <ParallaxLayer offset={0} speed={1} factor={1} className="z-40">
+                    <ParallaxLayer offset={0} speed={-1.9} factor={2} className="z-40">
                         <div className="h-96 w-96 bg-blue-600 border-none rounded-full" style={{marginLeft: "85%", marginTop: "-10%"}}></div>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={1.5} factor={1} className="z-20">
-                        <div className="h-96 w-96 bg-blue-800 border-none rounded-full" style={{marginLeft: "85%", marginTop: "-0%"}}></div>
+                    <ParallaxLayer offset={0} speed={-1.95} factor={2} className="z-20">
+                        <div className="h-96 w-96 bg-blue-800 border-none rounded-full" style={{marginLeft: "85%", marginTop: "0%"}}></div>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={1} factor={1} className="z-30">
+                    <ParallaxLayer offset={0} speed={-1.95} factor={2} className="z-30">
                         <div className="h-96 w-96 bg-blue-400 border-none rounded-full" style={{marginLeft: "70%", marginTop: "-10%"}}></div>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={1} factor={1} className="z-10">
+                    <ParallaxLayer offset={0} speed={-1.95} factor={2} className="z-10">
                         <div className="h-96 w-96 bg-green-800 border-none rounded-full " style={{marginLeft: "75%", marginTop: "-2.5%"}}></div>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={1} factor={1} className="z-0">    
+                    <ParallaxLayer offset={0} speed={-1.95} factor={2} className="z-0">    
                         <div className="h-96 w-96 bg-green-400 border-none rounded-full" style={{marginLeft: "60%", marginTop: "-7.5%"}}></div>
                     </ParallaxLayer>
 
                     
 
-                    <ParallaxLayer offset={0} speed={1} factor={0} className="z-50">
-                        <div className="h-96 w-96 bg-green-400 border-none rounded-full" style={{marginLeft: "37.5%", marginTop: "-7.5%"}}></div>
+                    <ParallaxLayer offset={0} speed={-2.22} factor={0} className="z-20">
+                        <div className="h-96 w-96 bg-green-800 border-none rounded-full" style={{marginLeft: "24%", marginTop: "-15.5%"}}></div>
+                    </ParallaxLayer>
+                    <ParallaxLayer offset={0} speed={-2.3} factor={0} className="z-10">
+                        <div className="h-96 w-96 bg-blue-300 border-none rounded-full" style={{marginLeft: "37.5%", marginTop: "-17.5%"}}></div>
+                    </ParallaxLayer>
+                    <ParallaxLayer offset={0} speed={-2.22} factor={0} className="z-20">
+                        <div className="h-96 w-96 bg-green-800 border-none rounded-full" style={{marginLeft: "51%", marginTop: "-15.5%"}}></div>
                     </ParallaxLayer>
 
 
 
-                    <ParallaxLayer offset={0} speed={1} factor={0}>
-                        <div className="h-96 w-96 bg-blue-600 border-none rounded-full"></div>
+                    <ParallaxLayer offset={0} speed={-1.9} factor={2} className="z-40">
+                        <div className="h-96 w-96 bg-blue-600 border-none rounded-full" style={{marginLeft: "-10%", marginTop: "-10%"}}></div>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={1} factor={0}>
-                        <div className="h-96 w-96 bg-blue-800 border-none rounded-full"></div>
+                    <ParallaxLayer offset={0} speed={-1.95} factor={2} className="z-20">
+                        <div className="h-96 w-96 bg-blue-800 border-none rounded-full" style={{marginLeft: "-10%", marginTop: "0%"}}></div>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={1} factor={0}>
-                        <div className="h-96 w-96 bg-blue-400 border-none rounded-full"></div>
+                    <ParallaxLayer offset={0} speed={-1.95} factor={2} className="z-30">
+                        <div className="h-96 w-96 bg-blue-400 border-none rounded-full" style={{marginLeft: "7.5%", marginTop: "-10%"}}></div>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={1} factor={0}>
-                        <div className="h-96 w-96 bg-green-800 border-none rounded-full"></div>
+                    <ParallaxLayer offset={0} speed={-1.95} factor={2} className="z-10">
+                        <div className="h-96 w-96 bg-green-800 border-none rounded-full" style={{marginLeft: "0%", marginTop: "-2.5%"}}></div>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={1} factor={0}>
-                        <div className="h-96 w-96 bg-green-400 border-none rounded-full"></div>
+                    <ParallaxLayer offset={0} speed={-1.95} factor={2} className="z-0">
+                        <div className="h-96 w-96 bg-green-400 border-none rounded-full" style={{marginLeft: "15%", marginTop: "-7.5%"}}></div>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={1} factor={0}>
-                        <h1 className="justify-self-center text-9xl text-blue-600 z-50">O nas</h1>
+                    <ParallaxLayer offset={0} speed={-1.5} factor={1} className="z-40">
+                        <h1 className="justify-self-center text-9xl text-blue-600 z-50" style={{marginLeft: "40%", marginTop: "10%"}}>O nas</h1>
                     </ParallaxLayer>
 
                 </div>
