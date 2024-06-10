415b4105d07767722f620beeb7ad95227991f547
afflorld <sl.adamec@gmail.com>
2024-06-10 22:49:36 +0200

    modified:   app/routes/_index.tsx 	modified:   app/components/about.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,7 +5,6 @@ import {Slideshow} from "~/components/slideshow";
 import { About } from "~/components/about";
 import { useRef, useEffect, useState } from "react";
 
-
 export const meta: MetaFunction = () => {
   return [
     { title: "Znojmo" },
@@ -47,26 +46,26 @@ export default function Index() {
   return (
     <div>
       <Header isVisible={showHeader}/>
-        <div className="desktop bg-white w-full h-full relative select-none ">
-          <div className="hide">
-            <div className="logo absolute translate-x-12 translate-y-6 border-none rounded-full overflow-hidden z-10 shadow-xl">
-              <img className="w-36 h-36" src="/images/logo/logo.png"/>
-            </div>
-            <div className="absolute right-12 translate-y-14 min-2xl:translate-y-16 shadow-xl border-none rounded-3xl overflow-hidden bg-white z-10">
-              <nav className="flex flex-wrap flex-row list-none min-2xl:text-xl text-base">
-                  <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">O nas</li></Link>
-                  <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Zavodni plavci</li></Link>
-                  <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Kurzy a primetske tabory</li></Link>
-                  <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Treneri</li></Link>
-                  <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Kontakt</li></Link>
-                  <a href="https://www.facebook.com/TJ.plavani.Znojmo/" target="_blank"><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Facebook</li></a>
-                  <Link to={"/"}><li className="min-2xl:p-6 p-5 hover:underline cursor-pointer transition-all"><div className="w-6 h-6"></div></li></Link>
-              </nav>
-            </div>
+      <div className="desktop bg-white w-full h-full relative select-none ">          
+        <div className="hide">
+          <div className="logo absolute translate-x-12 translate-y-6 border-none rounded-full overflow-hidden z-10 shadow-xl">
+            <img className="w-36 h-36" src="/images/logo/logo.png"/>
           </div>
-          <Slideshow />
-          <About/>
+          <div className="absolute right-12 translate-y-14 min-2xl:translate-y-16 shadow-xl border-none rounded-3xl overflow-hidden bg-white z-10">
+            <nav className="flex flex-wrap flex-row list-none min-2xl:text-xl text-base">
+                <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">O nas</li></Link>
+                <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Zavodni plavci</li></Link>
+                <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Kurzy a primetske tabory</li></Link>
+                <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Treneri</li></Link>
+                <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Kontakt</li></Link>
+                <a href="https://www.facebook.com/TJ.plavani.Znojmo/" target="_blank"><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Facebook</li></a>
+                <Link to={"/"}><li className="min-2xl:p-6 p-5 hover:underline cursor-pointer transition-all"><div className="w-6 h-6"></div></li></Link>
+            </nav>
+          </div>
         </div>
+        <Slideshow />
+        <About/>
+      </div>
     </div>
   );
 } 
