7689576b47ab78e8633382676141129dcc25295d
afflorld <sl.adamec@gmail.com>
2024-06-10 12:38:49 +0200

    modified:   app/components/slideshow.tsx 	new file:   app/styles/slideshow.css

--- app/components/slideshow.tsx
+++ app/components/slideshow.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState} from "react";
-
+import "../styles/slideshow.css"
 
 export function Slideshow(){
 
@@ -26,21 +26,28 @@ export function Slideshow(){
         },
     ]
 
-    
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const slideWidth = 100;
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+        }, 4000);
+
+        return () => clearInterval(interval);
+    }, [data.length]);
     
     return(
     
-        <div className="relative z-0 w-screen h-screen grid overflow-y-hidden">
-            <div className="w-screen flex flex-row flex-nowrap">
+        <div className="relative z-0 w-screen h-screen grid overflow-hidden">
+            <div className="slider w-screen flex flex-row flex-nowrap transition-transform " style={{ transform: `translateX(-${currentIndex * slideWidth}%)`}}>
                 {data.map((d:any) => (
-                    <img className="slide w-screen h-full" src={d.img}/> 
+                    <img key={d.id} className="slide w-screen h-full" src={d.img}/> 
                 ))}
             </div>
             <div className="absolute bottom-0 z-10 flex flex-row flex-nowrap justify-self-center">
-                {data.map((d:any) => (
-                    <div className="dot w-8 h-8 m-2 bg-gray-800 border-2 rounded-3xl cursor-pointer hover:bg-blue-600 transition-all shadow-xl">
-                    </div>
+                {data.map((d:any, index:any) => (
+                      <div key={d.id} className={`dot w-8 h-8 m-2 bg-gray-800 border-2 rounded-full cursor-pointer hover:bg-blue-600 transition-all ${index === currentIndex ? "bg-blue-600" : "bg-gray-800"}`} onClick={() => setCurrentIndex(index)}></div>
                 ))}
             </div>
         </div>
