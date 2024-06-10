b727d3926027f5b90498f107cc4502641afbd4b0
afflorld <sl.adamec@gmail.com>
2024-06-10 19:26:26 +0200

    modified:   app/components/header.tsx 	modified:   app/routes/_index.tsx 	modified:   app/styles/header.css

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,15 +5,18 @@ export function Header(){
 
     return(
 
-        <div className="header absolute right-12 translate-y-14 min-2xl:translate-y-16 shadow-xl border-none rounded-3xl overflow-hidden bg-white z-10">
-            <nav className="flex flex-wrap flex-row list-none min-2xl:text-xl text-base">
-                <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">O nas</li></Link>
-                <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Zavodni plavci</li></Link>
-                <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Kurzy a primetske tabory</li></Link>
-                <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Treneri</li></Link>
-                <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Kontakt</li></Link>
-                <a href="https://www.facebook.com/TJ.plavani.Znojmo/" target="_blank"><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Facebook</li></a>
-                <Link to={"/"}><li className="min-2xl:p-6 p-5 hover:underline cursor-pointer transition-all"><div className="w-6 h-6"></div></li></Link>
+        <div className="header sticky w-full h-32 overflow-hidden bg-white z-10 grid items-center"> 
+            <div className="fixed">
+                <img className="ml-2 h-28 w-28" src="/images/logo/logo.png"/>
+            </div>
+            <nav className="flex flex-wrap flex-row list-none fixed right-0 text-lg">
+                <Link to={""}><li className="p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">O nas</li></Link>
+                <Link to={""}><li className="p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Zavodni plavci</li></Link>
+                <Link to={""}><li className="p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Kurzy a primetske tabory</li></Link>
+                <Link to={""}><li className="p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Treneri</li></Link>
+                <Link to={""}><li className="p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Kontakt</li></Link>
+                <a href="https://www.facebook.com/TJ.plavani.Znojmo/" target="_blank"><li className="p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Facebook</li></a>
+                <Link to={"/"}><li className="p-5 hover:underline cursor-pointer transition-all"><div className="w-7 h-7"></div></li></Link>
             </nav>
         </div>
 
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@netlify/remix-runtime";
 import {Header} from "~/components/header";
+import { Link } from "@remix-run/react";
 import {Slideshow} from "~/components/slideshow";
 import { About } from "~/components/about";
 import { Parallax, ParallaxLayer, IParallax} from "@react-spring/parallax";
@@ -19,62 +20,29 @@ export default function Index() {
 
   return (
     <div>
-      <Parallax pages={3} ref={parallax}>
+      <Header/>
+      <Parallax pages={4} ref={parallax}>
         <div className="desktop bg-white w-full h-full relative select-none overflow-hidden">
           <ParallaxLayer offset={0} factor={1} speed={0}>
-            <div className="logo absolute translate-x-12 translate-y-6 border-none rounded-full overflow-hidden z-10 shadow-xl">
+            <div className="logo absolute translate-x-12 translate-y-6 border-none rounded-full overflow-hidden z-10 shadow-xl" style={{display:"none"}}>
               <img className="w-36 h-36" src="/images/logo/logo.png"/>
             </div>
-            <Header />
+            
+              <div className="absolute right-12 translate-y-14 min-2xl:translate-y-16 shadow-xl border-none rounded-3xl overflow-hidden bg-white z-10" style={{display:"none"}}>
+                <nav className="flex flex-wrap flex-row list-none min-2xl:text-xl text-base">
+                    <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">O nas</li></Link>
+                    <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Zavodni plavci</li></Link>
+                    <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Kurzy a primetske tabory</li></Link>
+                    <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Treneri</li></Link>
+                    <Link to={""}><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Kontakt</li></Link>
+                    <a href="https://www.facebook.com/TJ.plavani.Znojmo/" target="_blank"><li className="min-2xl:p-6 p-5 hover:bg-blue-500 hover:text-white hover:underline cursor-pointer transition-all">Facebook</li></a>
+                    <Link to={"/"}><li className="min-2xl:p-6 p-5 hover:underline cursor-pointer transition-all"><div className="w-6 h-6"></div></li></Link>
+                </nav>
+              </div>
+              
             <Slideshow />
           </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-2} factor={2} className="z-0">
-                <div className="bg-blue-200 w-scren h-screen relative -top-2/4"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-1.9} factor={2} className="z-40">
-                <div className="h-96 w-96 bg-blue-600 border-none rounded-full -right-40 -top-40 absolute"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-1.95} factor={2} className="z-20">
-                <div className="h-96 w-96 bg-blue-800 border-none rounded-full -right-40 -top-0 absolute"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-1.95} factor={2} className="z-30">
-                <div className="h-96 w-96 bg-blue-400 border-none rounded-full right-32 -top-40 absolute"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-1.95} factor={2} className="z-10">
-                <div className="h-96 w-96 bg-green-800 border-none rounded-full right-10 -top-16 absolute"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-1.95} factor={2} className="z-0">    
-                <div className="h-96 w-96 bg-green-400 border-none rounded-full right-60 -top-32 absolute"></div>
-            </ParallaxLayer>
-            
-            <ParallaxLayer offset={1} speed={-2.2} factor={2} className="z-20">
-                <div className="h-96 w-96 bg-green-800 border-none rounded-full left-96 -top-64 absolute"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-2.3} factor={2} className="z-30">
-                <div className="h-96 w-96 bg-blue-300 border-none rounded-full absolute m-auto left-0 right-0 -top-72"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-2.225} factor={2} className="z-20">
-                <div className="h-96 w-96 bg-green-800 border-none rounded-full right-96 -top-64 absolute"></div>
-            </ParallaxLayer>
 
-            <ParallaxLayer offset={1} speed={-1.9} factor={2} className="z-40">
-                <div className="h-96 w-96 bg-blue-600 border-none rounded-full -left-40 -top-40 absolute"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-1.95} factor={2} className="z-20">
-                <div className="h-96 w-96 bg-blue-800 border-none rounded-full -left-40 -top-0 absolute"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-1.95} factor={2} className="z-30">
-                <div className="h-96 w-96 bg-blue-400 border-none rounded-full left-32 -top-40 absolute"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-1.95} factor={2} className="z-10">
-                <div className="h-96 w-96 bg-green-800 border-none rounded-full left-10 -top-16 absolute"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-1.95} factor={2} className="z-0">
-                <div className="h-96 w-96 bg-green-400 border-none rounded-full left-60 -top-32 absolute"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={-2.15} factor={2} className="z-40 ">
-                <h1 className="justify-self-center text-9xl text-blue-600 absolute m-auto -translate-x-1/2 left-1/2 top-40">O nas</h1>
-            </ParallaxLayer>
         </div>
       </Parallax>
     </div>
