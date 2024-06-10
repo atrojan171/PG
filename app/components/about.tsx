d1466307995eb37772e8d93cea4d88d13fc01037
afflorld <sl.adamec@gmail.com>
2024-06-10 18:38:34 +0200

    modified:   app/components/about.tsx 	modified:   app/routes/_index.tsx

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -33,13 +33,13 @@ export function About(){
                         <div className="h-96 w-96 bg-green-400 border-none rounded-full right-60 -top-32 absolute"></div>
                     </ParallaxLayer>
                     
-                    <ParallaxLayer offset={0} speed={-2.22} factor={0} className="z-20">
+                    <ParallaxLayer offset={0} speed={-2.2} factor={2} className="z-20">
                         <div className="h-96 w-96 bg-green-800 border-none rounded-full left-96 -top-64 absolute"></div>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={-2.3} factor={0} className="z-30">
+                    <ParallaxLayer offset={0} speed={-2.3} factor={2} className="z-30">
                         <div className="h-96 w-96 bg-blue-300 border-none rounded-full absolute m-auto left-0 right-0 -top-72"></div>
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={-2.22} factor={0} className="z-20">
+                    <ParallaxLayer offset={0} speed={-2.225} factor={2} className="z-20">
                         <div className="h-96 w-96 bg-green-800 border-none rounded-full right-96 -top-64 absolute"></div>
                     </ParallaxLayer>
 
