7e20682183b76f7b5fcee554749aa5055d0edc7f
afflorld <sl.adamec@gmail.com>
2024-06-11 14:55:24 +0200

    modified:   app/components/about.tsx 	modified:   app/routes/_index.tsx

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -13,13 +13,7 @@ export function About(){
         <div>
             <Parallax pages={2} ref={parallax} className="no-scrollbar" style={{backgroundColor:"#FAFAFA"}}>
                 <ParallaxLayer factor={1.5} offset={0} speed={-2}>
-                    <div className="about z-0 absolute -top-52 left-7 w-6/12 h-4/6 scale-75 rounded-full shadow-xl"></div>
-                </ParallaxLayer>
-                <ParallaxLayer factor={1.5} offset={0} speed={-2}>
-                    <div className="about z-0 absolute -top-52 left-7 w-6/12 h-4/6 scale-75 rounded-full shadow-xl"></div>
-                </ParallaxLayer>
-                <ParallaxLayer factor={1.5} offset={0} speed={-2}>
-                    <div className="about z-0 absolute -top-52 left-7 w-6/12 h-4/6 scale-75 rounded-full shadow-xl"></div>
+                    <div className="about z-10 absolute -top-52 left-0 w-6/12 h-4/6 scale-75 rounded-full shadow-xl"></div>
                 </ParallaxLayer>
             </Parallax>
         </div>
