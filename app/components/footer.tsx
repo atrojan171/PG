8e89cad6441f33bafb9e23b4f2d35ae4f3f050f9
afflorld <sl.adamec@gmail.com>
2024-06-11 21:59:07 +0200

    modified:   app/components/footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ export function Footer(){
 
     return(
 
-        <div className="w-[240%] h-96 z-90 -ml-96 rounded-t-[100%] bg-gradient-to-b from-blue-400 via-blue-500 to-blue-600 relative">
+        <div className="w-[240%] h-96 z-90 -ml-96 rounded-t-[100%] bg-gradient-to-b from-blue-300 via-blue-500 to-blue-600 relative">
             
         </div>
     );
